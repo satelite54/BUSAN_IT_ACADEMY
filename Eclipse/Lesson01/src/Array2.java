9bba70a1c8388c8dd5a23fa449e5cdacc48adbba
satelite123 <satelite12@naver.com>
2020-11-02 19:13:52 +0900

    [자료구조] - java 배열 최대값 합 구하기

---
+++ Eclipse/Lesson01/src/Array2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int[] nArray = new int[n];
+		for(int i = 0; i < n; i++) {
+			nArray[i] = kb.nextInt();
+		}
+		kb.close();
+		
+		int sum = 0, max = nArray[0];
+		for(int j = 0; j < n; j++) {
+			sum += nArray[j];
+			if(nArray[j] > max) max = nArray[j];
+		}
+		System.out.println("The sum is" + sum + "and the maximum is" + max);	
+	}
+
+}

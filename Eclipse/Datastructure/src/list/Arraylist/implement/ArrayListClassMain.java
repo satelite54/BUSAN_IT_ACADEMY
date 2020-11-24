378ae75213b04dea026fb6a4c3485252e05418cd
satelite123 <satelite12@naver.com>
2020-11-24 19:19:34 +0900

    [일기]  - 20201124

--- Eclipse/Datastructure/src/list/Arraylist/implement/ArrayListClassMain.java
+++ Eclipse/Datastructure/src/list/Arraylist/implement/ArrayListClassMain.java
@@ -1,13 +1,19 @@
 package list.Arraylist.implement;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ArrayListClassMain {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.next();
-        String str = String.format("%x", num);
-        str.toUpperCase();
-        System.out.printf(str);
+        long a = sc.nextLong();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        for(int i = 0; i < c - 1; i++) {
+            a *= b;
+            if(i == c - 2)
+                System.out.printf("%d", a);
+        }
     }
 }

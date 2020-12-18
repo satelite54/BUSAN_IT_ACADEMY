4328021d7cfa0f001cef18000e46ba323a94cda9
satelite54 <taehune94@gmail.com>
2020-12-19 00:09:46 +0900

    [Java]  - syncronized

--- Eclipse/Java_Middel_class/src/java_temp/MusicBox.java
+++ Eclipse/Java_Middel_class/src/java_temp/MusicBox.java
@@ -1,7 +1,10 @@
 package java_temp;
 
 public class MusicBox {
-	public void playMusicA() {
+	public synchronized void playMusicA() { // 여러개의 객체들이 공유객체를 같이 사용할 경우
+		// 먼저 실행 되면 해당 객체의 사용권을 얻게 된다.
+		// synchronized 키워드가 이런 역할을 한다.
+		// 실행이 되면 모니터링 락을 얻게 된다.
 		for(int i = 0; i < 10; i++) {
 			System.out.println("신나는 음악!!!");
 			try {
@@ -12,7 +15,7 @@ public class MusicBox {
 			}
 		}
 	}
-	public void playMusicB() {
+	public synchronized void playMusicB() {
 		for(int i = 0; i < 10; i++) {
 			System.out.println("슬픈 음악 !!!");
 			try {
@@ -23,9 +26,12 @@ public class MusicBox {
 			}
 		}
 	}
-	public void playMusicC() {
+	public void playMusicC() {// 다른 스레드들이 모니터링 락을 획득해도 여기는 synchronized 키워드가 없어 자유롭게 실행된다.
 		for(int i = 0; i < 10; i++) {
-			System.out.println("카페 음악!!!");
+			synchronized (this) { //현재 객체가 모니터링 락을 가질 수 있을 때 모니터링을 가지게 해서 동기화 하겠다라는 뜻.
+				System.out.println("카페 음악!!!");
+			}			
+//			System.out.println("카페 음악!!!");
 			try {
 				Thread.sleep((int)(Math.random()*100));
 			} catch (InterruptedException e) {
@@ -34,4 +40,5 @@ public class MusicBox {
 			}
 		}
 	}
+	// 부분부분만 synchronized가 가능하다.
 }

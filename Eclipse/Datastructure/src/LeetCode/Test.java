4382533c2bd8ad914c7a421d7e6e90b957284722
satelite54 <taehune94@gmail.com>
2020-12-28 09:13:49 +0900

    [Java]  - 키패드 알고리즘

--- Eclipse/Datastructure/src/LeetCode/Test.java
+++ Eclipse/Datastructure/src/LeetCode/Test.java
@@ -1,5 +1,6 @@
 package LeetCode;
 
+import java.awt.RenderingHints.Key;
 import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -12,8 +13,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		long lg = 500000000;
-		solution27My(lg);
+		int[] nAry = {4, 5, 3, 2, 1};
+		solution28My(nAry);
 	}
 	public static int reversInteger (int x) {
 		// dwadwa
@@ -1165,6 +1166,63 @@ public class Test {
 		}
 		return nAry;
 	}
+    public  String solution(int [] n, String hand) {
+        StringBuilder bd = new StringBuilder();
+
+        int leftLocation = 10;
+        int rightLocation = 12;
+
+        for(int number : n) {
+            if(number ==1 || number == 4 || number == 7) {
+                bd.append("L");
+                leftLocation = number;
+            }else if(number == 3 || number == 6 || number == 9) {
+                bd.append("R");
+                rightLocation = number;
+            }else { // 2 5 8 0 
+                int distanceL = getDist(leftLocation, number);
+                int distanceR = getDist(rightLocation, number);
+
+                if(distanceL > distanceR) {
+                    bd.append("R");
+                    rightLocation = number;
+                }else if(distanceL < distanceR) {
+                    bd.append("L");
+                    leftLocation = number;
+                }else {
+                    if(hand.equals("right")) {
+                        bd.append("R");
+                        rightLocation = number;
+                    }else {
+                        bd.append("L");
+                        leftLocation = number;
+                    }
+                }
+
+            }
+        }
+        return bd.toString();
+    }
+
+    public static int getDist(int location, int number) {
+
+        if(number == 0) {
+            number = 11;
+        }
+
+        if(location == 0) {
+            location = 11;
+        }
+
+        int locationX = (location-1) / 3;
+        int locationY = (location-1) % 3;
+
+        int numberX = (number-1) / 3;
+        int numberY = (number-1) % 3;
+
+        return Math.abs(locationX-numberX) + Math.abs(locationY - numberY);
+
+    }
 }
 
 

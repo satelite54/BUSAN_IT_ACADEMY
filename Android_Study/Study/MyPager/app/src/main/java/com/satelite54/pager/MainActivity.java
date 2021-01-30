1eabb4becf95b4a3f962a1e72579d544ff2c647f
satelite54 <taehune94@gmail.com>
2021-01-30 16:43:16 +0900

    [Android]  - Viewpager,  Drawer 학습

--- Android_Study/Study/MyPager/app/src/main/java/com/satelite54/pager/MainActivity.java
+++ Android_Study/Study/MyPager/app/src/main/java/com/satelite54/pager/MainActivity.java
@@ -1,23 +1,70 @@
 package com.satelite54.pager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    class MyPagetAdapter extends FragmentStateAdapter {
-        ArrayList<Fragment> items = new ArrayList<>();
 
-        public MyPagetAdapter
+        pager = findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+
+        MyPagetAdapter adapter = new MyPagetAdapter(getSupportFragmentManager());
+        Fragment1 fragment1 = new Fragment1();
+        adapter.addItem(fragment1);
+
+        Fragment2 fragment2 = new Fragment2();
+        adapter.addItem(fragment2);
+        Fragment3 fragment3 = new Fragment3();
+        adapter.addItem(fragment3);
+
+        pager.setAdapter(adapter);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pager.setCurrentItem(1);
+            }
+        });
+    }
+    class MyPagetAdapter extends FragmentPagerAdapter {
+        ArrayList<Fragment> items = new ArrayList<>();
+        // 여러개의 Fragment를 담기 위한 리스트
+
+        public MyPagetAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addItem(Fragment item) {
+            items.add(item);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return items.get(position); //FragmentPagerAdapter에서 필요한 Fragment 리턴
+        }
+
+        @Override
+        public int getCount() {
+            return items.size(); // Fragment 갯수 반환
+        }
     }
 }

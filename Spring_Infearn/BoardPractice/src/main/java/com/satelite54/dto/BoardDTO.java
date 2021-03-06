157c948086fef683251880efbbfc4ed990f7063c
satelite54 <taehune94@gmail.com>
2021-03-06 17:58:45 +0900

    [Spring]  - 게시판 연습을 위한 commuity.jsp, js, css 파일 추가  - BoardDTO 추가  - lombok 라이브러리 추가

---
+++ Spring_Infearn/BoardPractice/src/main/java/com/satelite54/dto/BoardDTO.java
@@ -0,0 +1,24 @@
+package com.satelite54.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardDTO {
+	
+	private int BNum;
+	private String BTitle;
+	private String BContent;
+	private String BDate;
+	private String id;
+	private int authority;
+	private int views;
+	
+	@Override
+	public String toString() {
+		return "Board [BNum=" + BNum + ", BTitle=" + BTitle + ", BContent=" + BContent + ", BDate=" + BDate + ", id="
+				+ id + ", authority=" + authority + ", views=" + views + "]";
+	}
+}
+

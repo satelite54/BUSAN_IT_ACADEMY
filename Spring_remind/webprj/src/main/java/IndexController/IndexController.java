7e4283ec8ddf1b3ac789caf723cb0674ae82a4e8
satelite54 <taehune94@gmail.com>
2021-04-15 11:48:38 +0900

    [Spring]  - DispatcherServlet이 호출하는 handleRequest 직접 구현

--- Spring_remind/webprj/src/main/java/IndexController/IndexController.java
+++
@@ -1,5 +0,0 @@
-package IndexController;
-
-public class IndexController {
-
-}
---
+++ Spring_remind/webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.newlecture.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller {
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("IndexController~~~");
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("data", "HelloSpring MVC~");
+		mv.setViewName("index.jsp");
+		return mv;
+	}
+}
